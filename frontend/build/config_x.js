076a14fa5a27288b466e19d17253d70ec9b2edfe
Markus Kolp <markus.kolp-extern@messe-muenchen.de>
2023-10-16 14:22:41 +0200

    nun auch mit build dateien (frontend)

---
+++ frontend/build/config_x.js
@@ -0,0 +1,14 @@
+// api
+var BASE_URL = '<API_URL_GOES_HERE>';
+
+// header and footer
+var APP_TITLE = ''; //Data & BI';
+var HEADER_TITLE = ''; //Data & BI';
+var FOOTER = ''; //© 2023 created by Data & BI';
+
+// theme
+var THEME_COLOR_PRIMARY = 'rgb(106, 145, 206)';
+var THEME_COLOR_SUCCESS = '#91c311';
+var THEME_COLOR_ERROR = '#b31013';
+var THEME_COLOR_INFO = '#3b80e0';
+var THEME_FONT_SIZE = 13;
