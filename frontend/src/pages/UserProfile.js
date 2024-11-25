183bf73e7aa86ea6ffe7ef7be260ff0c1d080735
Markus Kolp <markus.kolp-extern@messe-muenchen.de>
2024-11-25 17:28:16 +0100

    check if token expires

--- frontend/src/pages/UserProfile.js
+++ frontend/src/pages/UserProfile.js
@@ -11,6 +11,8 @@ const UserProfile = (props) => {
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState([]); // profile data
+  const [errorMessage, setErrorMessage] = useState('');
+  const [errorDetail, setErrorDetail] = useState('');
 
   useEffect(() => {
     initializeApp();
@@ -27,9 +29,17 @@ const UserProfile = (props) => {
       }
     ).catch(
       function (error) {
-        setError(true);
         setLoading(false);
-        message.error('Es gab einen Fehler beim Laden der Profildaten.');
+        setError(true);
+        if (error.response.status === 401) {
+          props.removeToken()
+          message.error('Session ist abgelaufen');
+        } else {
+          setErrorMessage('Es gab einen Fehler beim Laden der Profildaten.');
+          setErrorDetail((typeof error.response.data.message !== 'undefined' && error.response.data.message ? error.response.data.message : "") + (typeof error.response.data.detail !== 'undefined' && error.response.data.detail ? ": " + error.response.data.detail : ""));
+         }
+        console.log(error);
+        console.log(error.response.data.message);
       }
     );
   };
@@ -39,18 +49,30 @@ const UserProfile = (props) => {
     <React.Fragment>
       {loading ? (
           <LoadingMessage />
-        ) : ( 
-          <React.Fragment>
-            <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
-              <Title level={3}>{data.fullname ? data.fullname + " (" + data.username + ")" : data.username}</Title> 
-              <Link href={"mailto:"+data.email} >{data.email}</Link> 
-              <Title level={5}>Rolle:</Title> 
-              <Text>{data.role}</Text> 
-              <Title level={5}>Gruppen:</Title> 
-              {data.groups && data.groups.map((group) => {console.log(group);return <Text>{group.name}</Text>})}
-            </Space>
-          </React.Fragment>
-        )}
+        ) : 
+        (
+          error ? 
+          (
+            <Alert
+              message={errorMessage}
+              description={errorDetail}
+              type="error"
+              showIcon
+            />
+          ) : ( 
+            <React.Fragment>
+              <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
+                <Title level={3}>{data.fullname ? data.fullname + " (" + data.username + ")" : data.username}</Title> 
+                <Link href={"mailto:"+data.email} >{data.email}</Link> 
+                <Title level={5}>Rolle:</Title> 
+                <Text>{data.role}</Text> 
+                <Title level={5}>Gruppen:</Title> 
+                {data.groups && data.groups.map((group) => {console.log(group);return <Text>{group.name}</Text>})}
+              </Space>
+            </React.Fragment>
+          )
+        )
+      }
     </React.Fragment>
   );
 
